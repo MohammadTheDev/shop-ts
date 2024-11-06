34e47f90586536d26a77fb6156daba3f302166ca
Mohammad Arab <mohammad.arab.dev@gmail.com>
2024-11-06 12:50:59 +0330

    Update Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { CartContext } from "../context/cartContext";
