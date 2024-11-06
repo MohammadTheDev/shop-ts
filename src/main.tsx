1ebd9d7a93d30d25d9b71659c054f9dce6eef6c1
Mohammad Arab <mohammad.arab.dev@gmail.com>
2024-11-06 12:46:14 +0330

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Cart from "./Pages/Cart";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
+   <React.StrictMode>
   <BrowserRouter>
     <App />
-    {/* <Cart /> */}
   </BrowserRouter>
-  // </React.StrictMode>
+   </React.StrictMode>
 );
